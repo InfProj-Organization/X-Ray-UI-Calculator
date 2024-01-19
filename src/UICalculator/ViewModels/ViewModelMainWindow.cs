9dc6d82062c0cafa3dfcc27c871c55b7f642c445
OldSerpskiStalker <ictopiar@gmail.com>
2024-01-19 20:35:36 +0500

    First template app

---
+++ src/UICalculator/ViewModels/ViewModelMainWindow.cs
@@ -0,0 +1,36 @@
+﻿using DynamicData;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using static UICalculator.UIRelayCommand.UIRelayCommands;
+
+namespace UICalculator.ViewModels
+{
+    public class ViewModelMainWindow
+    {
+        public ICommand DragMoveApp { get; }
+
+        public ViewModelMainWindow()
+        {
+            DragMoveApp = new IRelayCommand(() =>
+            { Application.Current.MainWindow?.DragMove(); }, () => true);
+        }
+
+        [Reactive]
+        public int FontSizeProp { get; set; } = 18;
+
+        [Reactive]
+        public int FontSizeButtonsProp { get; set; } = 14;
+    }
+}
